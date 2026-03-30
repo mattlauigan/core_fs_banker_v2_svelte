d17e8d86f61dc57cafd08c94f7b88f964150ac35
mattlauigan <raymartlauigan@gmail.com>
2026-03-30 10:59:58 +0800

    installed tailwind css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
-  let { children } = $props();
-  import Header from "../components/header/Header.svelte";
-  // import Header from '$components/header/Header.css'
+	import './layout.css';
+
+	let { children } = $props();
+
+	import Header from "../components/header/Header.svelte";
+
+	// import Header from '$components/header/Header.css'
 </script>
 
 <Header />
-<div class="content">
-  {@render children()}
-</div>
+<div class="content">{@render children()}</div>
 
 <style>
-  /* @import url("header.css"); */
-  .content {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    margin-inline: auto;
-    overflow: hidden;
-    z-index: 1;
-    margin-top: 8vh;
-    padding-top: 1.5rem;
-  }
+	.content {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		margin-inline: auto;
+		overflow: hidden;
+		z-index: 1;
+		margin-top: 8vh;
+		padding-top: 1.5rem;
+	}
 </style>
