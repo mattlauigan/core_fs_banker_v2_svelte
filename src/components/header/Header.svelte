4c300553ff9de3738691d61a35faac0a8ba0306f
mattlauigan@mac <lauigan.mattt@gmail.com>
2026-04-08 23:07:17 +0800

    style: fix incorrect class name
    BREAKING CHANGE:
    - added change of user status when clicking Open Teller

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -111,52 +111,55 @@
       </div>
 
       {#if isUserPanel}
-        <div
-          class="_user_panel_popover"
-          onmouseleave={closeUserPanel}
-          role="button"
-          tabindex="0"
-        >
-          <div class="_user_panel_popover_content">
-            <ToggleSwitch
-              name="darkMode"
-              label="Dark Mode"
-              disabled={false}
-              checked={menuStates.darkmode}
-              onCheck={(checked) => {
-                console.log(checked); // boolean ✅
-              }}
-            />
-            <hr class="_user_panel_hr" />
-            <button class="text-left" onclick={() => {}}>Open Teller</button>
-            <a href="a"> Teller Journal </a>
-            <a href="b"> Previous Transaction Summary </a>
-            <hr class="_user_panel_hr" />
-            <a href="c"> Terminal </a>
-            <a href="d"> Branch </a>
-            <a href="e"> User </a>
-            <hr class="_user_panel_hr" />
-            <a href="f"> Current Business Date </a>
-            <a href="g"> Today Transaction </a>
-            <a href="h"> Host Status </a>
-            <a href="i"> Portal Status </a>
+      <div
+        class="_user_panel_popover"
+        onmouseleave={closeUserPanel}
+        role="button"
+        tabindex="0"
+      >
+        <div class="_user_panel_popover_content">
+          <ToggleSwitch
+            name="darkMode"
+            label="Dark Mode"
+            disabled={false}
+            checked={menuStates.darkmode}
+            onCheck={(checked) => {
+              console.log(checked); // boolean ✅
+            }}
+          />
+          <hr class="_user_panel_hr" />
+          <button class="text-left cursor-pointer" onclick={() => {
+            menuStates.online = !menuStates.online
+            console.log(menuStates.online)
+          }}>Open Teller</button>
+          <a href="a"> Teller Journal </a>
+          <a href="b"> Previous Transaction Summary </a>
+          <hr class="_user_panel_hr" />
+          <a href="c"> Terminal </a>
+          <a href="d"> Branch </a>
+          <a href="e"> User </a>
+          <hr class="_user_panel_hr" />
+          <a href="f"> Current Business Date </a>
+          <a href="g"> Today Transaction </a>
+          <a href="h"> Host Status </a>
+          <a href="i"> Portal Status </a>
 
-            <hr class="_user_panel_hr" />
-            <a href="j"> Change Password</a>
-            <a href="k"> View Login Record </a>
-            <p>Recent Visited Transaction</p>
-            <div class="_user_popover_frequent_container pl-6">
-              {#each frequentModules as module}
-                <a href={module.route} >
-                  {module.name}
-                </a>
-              {/each}
-            </div>
-            <hr class="_user_panel_hr" />
-            <a href="l" class="text-red-500"> Logout </a>
+          <hr class="_user_panel_hr" />
+          <a href="j"> Change Password</a>
+          <a href="k"> View Login Record </a>
+          <p>Recent Visited Transaction</p>
+          <div class="_user_popover_frequent_container pl-6">
+            {#each frequentModules as module}
+              <a href={module.route} >
+                {module.name}
+              </a>
+            {/each}
           </div>
+          <hr class="_user_panel_hr" />
+          <a href="l" class="text-red-500"> Logout </a>
         </div>
-      {/if}
+      </div>
+    {/if}
     </div>
   </div>
 
