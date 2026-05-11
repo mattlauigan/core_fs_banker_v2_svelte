2931ce868eca8ca658ba1359a47793173ef1f48c
COREWARE-MATT\lauig <mat.lauigan@corewaretech.com.ph>
2026-05-11 17:46:56 +0800

    feat: Added inputField.css import and TextField component to Hero page

---
+++ src/components/inputs/TextField.svelte
@@ -0,0 +1,37 @@
+<script lang='ts'>
+  type Props = {
+    id: string;
+    name: string;
+    label: string;
+    type?: string;
+    placeholder?: string;
+  };
+
+  let {
+    id,
+    name,
+    label = "Label",
+    type = "text",
+    placeholder = "asdasds",
+  }: Props = $props();
+</script>
+
+<div class="relative my-10 w-75">
+  <input
+    {type}
+    {id}
+    {name}
+    {placeholder}
+    class="peer h-12 w-full border-b border-accent-003 bg-transparent text-[12px] tracking-[1.5px] text-scripts outline-none transition-colors duration-300 focus:border-primary-500 pl-1.5"
+  />
+
+  <label
+    for="username"
+    class="pointer-events-none absolute left-0 top-1.5 text-[12px] tracking-[1.5px] text-accent-003 transition-all duration-300 ease-in-out peer-placeholder-shown:top-3 peer-placeholder-shown:text-[12px] peer-focus:-top-3 peer-focus:text-[11px] peer-focus:text-primary-500 peer-not-placeholder-shown:-top-3 peer-not-placeholder-shown:text-[11px]"
+    >{label}
+    <span
+      class="absolute bottom-0 left-0 h-0.5 w-full origin-left scale-x-0 bg-primary-500 transition-transform duration-300 ease-in-out peer-focus:scale-x-100 text-black"
+      >a</span
+    >
+  </label>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
-<script lang='ts'>
+<script lang="ts">
   import Hero from "$components/Hero.svelte";
-
+  import TextField from "$components/inputs/TextField.svelte";
 </script>
 
 <Hero />
+
+<TextField id='trial' name='trial' label='Trial 2' />
+
+<br />
