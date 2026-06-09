08017aea189a26edbcfab80f47973060ef01ff81
COREWARE-MATT\lauig <mat.lauigan@corewaretech.com.ph>
2026-06-09 19:07:51 +0800

    refactor(style): fix the conflict between DialogWindow and MegaMenu Modal

--- src/components/layouts/Header.svelte
+++ src/components/layouts/Header.svelte
@@ -18,10 +18,10 @@
     },
   });
 
-  let isScrolled = $state(false);
+  let scrolled = $state(false);
 
   function handleScroll() {
-    isScrolled = window.scrollY > 0;
+    scrolled = window.scrollY > 0;
   }
 
   let frequentModules = megamenu.frequentModules;
@@ -35,18 +35,13 @@
 
 <svelte:window onscroll={handleScroll} />
 
-<header
-  class={menuStates.category !== "default" || isUserPanel ? "shadow-none" : ""}
->
-  <div
-    class="_header_container
-    {!isScrolled ? '' : '_scrolled'}"
-  >
+<header class:shadow-none={menuStates.category !== "default" || isUserPanel}>
+  <div class="_header_container" class:_scrolled={scrolled}>
     <div class="pt-3">
       <span>
         <a href="/">
           <img
-            src={!isScrolled ? companyLogo : companyLogoDark}
+            src={!scrolled ? companyLogo : companyLogoDark}
             alt="Organization"
             class="h-14 w-auto"
           />
@@ -54,7 +49,11 @@
       </span>
     </div>
 
-    <Menu category={menuStates.category} root={menuStates.root} {isScrolled} />
+    <Menu
+      category={menuStates.category}
+      root={menuStates.root}
+      isScrolled={scrolled}
+    />
 
     <UserPanel
       isPopOver={menuStates.popover.userPanel}
