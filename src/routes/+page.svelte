983566a6c33aa9707682319ce8819ca6b9e6a6c1
mattlauigan@mac <lauigan.mattt@gmail.com>
2026-07-15 21:05:20 +0800

    chore: remove unnecessary items in dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,10 @@
 <script lang="ts">
-  import {
-    ConfirmationIcon,
-    CycleIcon,
-    DeleteIcon,
-    ErrorIcon,
-    InfoIcon,
-    NotificationIcon,
-    SearchIcon,
-    SettingsIcon,
-    SuccessIcon,
-    UserIcon,
-    WarningIcon,
-  } from "$components/primitives/icons";
-  import DialogWindow from "$components/layouts/DialogWindow.svelte";
-  import Hero from "$components/Hero.svelte";
-  import TextArea from "$components/primitives/TextArea.svelte";
-  import TextInput from "$components/primitives/TextInput.svelte";
-  import User from "$components/User.svelte";
-  import { ModalTypeEnum } from "$lib/ts/enums/modal";
   import CardContainer from "$components/primitives/CardContainer.svelte";
-  import PasswordInput from "$components/primitives/PasswordInput.svelte";
+  import Hero from "$components/Hero.svelte";
 </script>
 
 <CardContainer>
   <Hero />
-  <TextInput id="trial" name="trial" label="Trial 2" style="w-75" />
-  <div class="flex space-around w-full">
-    <ConfirmationIcon width={48} height={48} />
-    <CycleIcon width={48} height={48} />
-    <ErrorIcon width={48} height={48} />
-    <InfoIcon width={48} height={48} />
-    <NotificationIcon width={48} height={48} />
-    <SearchIcon width={48} height={48} />
-    <SettingsIcon width={48} height={48} />
-    <SuccessIcon width={48} height={48} />
-    <UserIcon width={48} height={48} />
-    <WarningIcon width={48} height={48} />
-
-    <DeleteIcon width={48} height={48} />
-  </div>
-  <br />
-
-  <PasswordInput
-    id="trial2"
-    name="trial2"
-    label="Password"
-    style="w-64"
-    required
-  />
-  <TextArea
-    id="trial2"
-    name="trial2"
-    label="Trial TextArea"
-    style="w-64"
-    required
-  ></TextArea>
-
-  <User />
 </CardContainer>
 
 
